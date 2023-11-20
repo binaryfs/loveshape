85c68ac682f7cfae12cc56b0e9e13388197d67b4
Fabian Staacke <github@myrk.de>
2023-11-20 19:52:02 +0100

    Disable unused LÖVE modules

--- conf.lua
+++ conf.lua
@@ -1,4 +1,11 @@
 function love.conf(t)
   t.window.title = "loveshape demo"
   t.window.msaa = 2
+
+  t.modules.audio = false
+  t.modules.joystick = false
+  t.modules.physics = false
+  t.modules.sound = false
+  t.modules.thread = false
+  t.modules.video = false
 end
